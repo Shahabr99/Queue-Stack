ee908ec380442bdf692e7f99fd4688608a1e4367
Shahabr99 <sean.rohani9@gmail.com>
2023-06-27 23:08:47 -0700

    stack done

--- stack.js
+++ stack.js
@@ -20,26 +20,50 @@ class Stack {
   /** push(val): add new value to end of the stack. Returns undefined. */
 
   push(val) {
-
+    const newNode = new Node(val);
+    if (this.isEmpty()) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.first.next = newNode;
+      this.first = newNode;
+    }
+    this.size++;
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
+    if (this.isEmpty()) {
+      throw new Error("Stack is empty");
+    }
+    const value = this.first.val;
+    if (this.first.next === null) {
+      this.first === null;
+      this.last === null;
+    } else {
+      this.first = this.first.next;
+    }
 
+    this.size--;
+    return value;
   }
 
   /** peek(): return the value of the first node in the stack. */
 
   peek() {
-
+    return this.first.val;
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    if (this.size === 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
